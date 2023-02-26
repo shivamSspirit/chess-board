0bee17917cdb07cbe724c89a0019947d6536c159
shivam <shivam.spirite3@outlook.com>
2023-02-26 16:05:57 +0530

    add meta tags

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,18 +2,18 @@ import React from 'react'
 import Chessboard from '../chessboard/Chessboard'
 import './main.css'
 
-function Main({theme,toggleTheme}) {
+function Main({ theme, toggleTheme }) {
   return (
     <div className='main-container' id={theme}>
-        <div className='header-container'>
+      <div className='header-container'>
         <h1 className='board-title'>Chess Board</h1>
-            <span className='theme-icon'>
-                {theme==='light'?  <img src='/moon.png' onClick={()=>toggleTheme()} className='icons' alt='icons'/>:  <img src='/sun.png' onClick={()=>toggleTheme()} className='icons' alt='icons'/>}         
-            </span>
-        </div>
-        <div className='chess-container'>
-        <Chessboard/>
-        </div>
+        <span className='theme-icon'>
+          {theme === 'light' ? <img src='/moon.png' onClick={() => toggleTheme()} className='icons' alt='icons' /> : <img src='/sun.png' onClick={() => toggleTheme()} className='icons' alt='icons' />}
+        </span>
+      </div>
+      <div className='chess-container'>
+        <Chessboard />
+      </div>
     </div>
   )
 }
